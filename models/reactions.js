50b90fef3610200ecc0444f9c8ae3776baacfdda
Brandon Kilgore <brandonk1023@gmail.com>
2023-05-31 17:55:34 -0400

    added model code into reaction.js

--- models/reactions.js
+++ models/reactions.js
@@ -1,5 +1,31 @@
 const { schema, Types} = require('mongoose')
 
-const reactionSchema = new Schema()
+const reactionSchema = new Schema({
+    reactionId:{
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 300,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    created: {
+        type: Date,
+        default: Date.now()
+    }
+},
+{
+    toJson: {
+        virtuals: true
+    },
+    _id: false
+}
+)
 
 module.exports = reactionSchema
