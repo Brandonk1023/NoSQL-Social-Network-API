3b462e2c8a8a2e3e30bfbb593f54cd896d94d924
Brandon Kilgore <brandonk1023@gmail.com>
2023-06-22 18:05:30 -0400

    began debugging code to get api running

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,4 +1,4 @@
-const { Schema, Types } = require('mongoose')
+const { Schema, Types, get, model } = require('mongoose')
 const reactionSchema = require('./reactions')
 
 const thoughtSchema = new Schema({
